23fe8f0d79ca2cc239ecef9d9bb34c1fe41fa1a6
Egha Kusuma <eghakusuma1@gmail.com>
2021-12-10 22:05:44 -0700

    update

--- server/index.js
+++ server/index.js
@@ -4,8 +4,12 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../outdoor.html"));
+  res.sendFile(path.join(__dirname, "../home.html"));
 });
+
+app.get("/outdoor", (req, res) => {
+    res.sendFile(path.join(__dirname, "../outdoor.html"));
+  });
 
 const port = process.env.PORT || 4005;
 
