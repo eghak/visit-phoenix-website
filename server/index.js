59870d0febe52af59862f327f0a740763c9d82aa
Egha Kusuma <eghakusuma1@gmail.com>
2021-12-13 10:30:12 -0700

    update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
 // app.get("/", (req, res) => {
