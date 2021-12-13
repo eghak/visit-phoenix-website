3182c6b60a6720f61e3ad4e6c83067fa8d5d5da5
Egha Kusuma <eghakusuma1@gmail.com>
2021-12-13 08:41:14 -0700

    update

--- server/index.js
+++ server/index.js
@@ -11,6 +11,14 @@ app.get("/homecss", (req, res) => {
     res.sendFile(path.join(__dirname, "../styleshome.css"));
   });
 
+  app.get("/outdoor", (req, res) => {
+    res.sendFile(path.join(__dirname, "../home.html"));
+  });
+  
+  app.get("/outdoorcss", (req, res) => {
+      res.sendFile(path.join(__dirname, "../stylesoutdoor.css"));
+    });
+
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
