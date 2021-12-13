226d5cfb42a9c8fa65ac7f48889751c59fb84dbe
Egha Kusuma <eghakusuma1@gmail.com>
2021-12-13 10:21:04 -0700

    update

--- server/index.js
+++ server/index.js
@@ -4,20 +4,24 @@ const path = require("path");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../home.html"));
-});
-
-app.get("/homecss", (req, res) => {
-    res.sendFile(path.join(__dirname, "../styleshome.css"));
+    res.sendFile(path.join(__dirname, "../public"));
   });
 
-  app.get("/outdoor", (req, res) => {
-    res.sendFile(path.join(__dirname, "../outdoor.html"));
-  });
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../home.html"));
+// });
+
+// app.get("/homecss", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../styleshome.css"));
+//   });
+
+//   app.get("/outdoor", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../outdoor.html"));
+//   });
   
-  app.get("/outdoorcss", (req, res) => {
-      res.sendFile(path.join(__dirname, "../stylesoutdoor.css"));
-    });
+//   app.get("/outdoorcss", (req, res) => {
+//       res.sendFile(path.join(__dirname, "../stylesoutdoor.css"));
+//     });
 
 const port = process.env.PORT || 4005;
 
